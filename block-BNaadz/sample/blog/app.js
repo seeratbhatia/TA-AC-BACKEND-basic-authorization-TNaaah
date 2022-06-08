61a6b52072b35ad78e84fda9ed58c720e52ac03b
seeratbhatia <b.seerat@gmail.com>
2022-06-09 00:19:47 +0530

    done

--- block-BNaadz/sample/blog/app.js
+++ block-BNaadz/sample/blog/app.js
@@ -1,36 +1,55 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var mongoose = require('mongoose');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var session = require("express-session");
+var MongoStore = require("connect-mongo");
+var flash = require("connect-flash");
+var auth = require("./middlewares/auth");
 
-var indexRouter = require('./routes/index');
-var articlesRouter = require('./routes/articles');
+require("dotenv").config();
+
+var indexRouter = require("./routes/index");
+var registerRouter = require("./routes/register");
+var articlesRouter = require("./routes/articles");
+var commentsRouter = require("./routes/comments");
+
+mongoose.connect("mongodb://localhost/article", (err) => {
+  console.log(err ? err : "Connected to Database");
+});
 
 var app = express();
 
-// connect to db
-mongoose.connect(
-  'mongodb://localhost/blogTwo',
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  (err) => {
-    console.log('Connected', err ? false : true);
-  }
-);
-
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/articles', articlesRouter);
+//add session
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: "mongodb://localhost/article" }),
+  })
+);
+
+app.use(flash());
+
+app.use(auth.registerInfo);
+
+app.use("/", indexRouter);
+app.use("/register", registerRouter);
+app.use("/articles", articlesRouter);
+app.use("/comments", commentsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -41,11 +60,11 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
